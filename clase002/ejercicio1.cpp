bf5e7958d4491ba019ba707ddcd67e2b1f6cd345
Johguxo <jgonzalesi@uni.pe>
2023-04-17 19:21:55 -0500

    Add new exercises

--- clase002/ejercicio1.cpp
+++ clase002/ejercicio1.cpp
@@ -8,11 +8,30 @@
 
   Si son más de diez, se les dará un 10% de descuento sobre el total de la compra; 
   si el número de zapatos es mayor de veinte pero menor de treinta, se le otorga un 20% de descuento; 
-  y si son más treinta zapatos se otorgará un 40% de descuento. El precio de cada zapato es de $80.
+  y si son más treinta zapatos se otorgará un 40% de descuento. El precio de cada zapato es de S/.80.
 
   Calcular el monto que debera pagar
 
   Input:
-  - Cantidad de zaptos que llevará
+  - Cantidad de zapatos que llevará
 */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  int number_shoes;
+  int monto;
+  cout << "Ingresar cantidad de zapatos: ";
+  cin >> number_shoes;
+  monto = number_shoes*80;
+  if (number_shoes >= 10) {
+    monto = monto*0.9;
+  } else if (number_shoes >= 20 && number_shoes <= 30) {
+    monto = monto*0.8;
+  } else if (number_shoes > 30) {
+    monto = monto*0.6;
+  }
+  cout << "El precio seria: " << monto;
+}
 
